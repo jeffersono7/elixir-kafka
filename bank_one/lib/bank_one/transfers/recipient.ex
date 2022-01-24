202cb9410f29369f96ed4120c9d25ed00dfe24db
Jefferson Farias <jefferson.farias7@gmail.com>
2022-01-24 09:05:47 -0300

    change to 50 partitions

--- bank_one/lib/bank_one/transfers/recipient.ex
+++ bank_one/lib/bank_one/transfers/recipient.ex
@@ -1,7 +1,7 @@
 defmodule BankOne.Transfers.Recipient do
   @topic "bank_one-transfer"
   @hosts [localhost: 9093]
-  @partitions 5
+  @partitions 50
 
   def send_transfers_to_kafka(transfers) do
     :ok = :brod.start_client(@hosts, :bank_one, _client_config = [])
--- create-topic.sh
+++ create-topic.sh
@@ -1,4 +1,4 @@
-docker exec -it kafka /bin/bash -c "kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 5 --replication-factor 1 --topic bank_one-transfer"
-docker exec -it kafka /bin/bash -c "kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 5 --replication-factor 1 --topic bank_two-transfer"
+docker exec -it kafka /bin/bash -c "kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 50 --replication-factor 1 --topic bank_one-transfer"
+docker exec -it kafka /bin/bash -c "kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 50 --replication-factor 1 --topic bank_two-transfer"
 
 echo "\nDONE\n"
